c75ca7b5416e90ad3d7c4508289e70555df2dee4
Ramosa Jummel Mark M. <ramosajummelmark@gmail.com>
2025-11-19 10:53:34 +0800

    1st

---
+++ src/main/java/com/ramosa/lab_7/Lab7Application.java
@@ -0,0 +1,13 @@
+package com.ramosa.lab_7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class   Lab7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramosa/lab_7/Lab7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramosa.lab_7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
